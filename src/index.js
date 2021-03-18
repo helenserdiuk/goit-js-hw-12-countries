095fa8089dfa20b83420a3473469b37992c49097
Elena S <76928777+helenserdiuk@users.noreply.github.com>
2021-03-18 09:16:57 +0200

    cleaning countriesList

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { alert, error } from '@pnotify/core/dist/PNotify.js';
 
+
 const refs = {
   input: document.querySelector('#input'),
   countriesList: document.querySelector('.country-list'),
@@ -15,18 +16,23 @@ const refs = {
 refs.input.addEventListener('input', debounce(onInput, 500));
 
 function onInput(e) {
+  if(!e.target.value){
+    refs.countriesList.innerHTML = '';
+    return;
+  }
   fetchCountries(e.target.value)
     .then(countries => countryMarkup(countries))
     .catch(err => error({ text: err.message }));
 }
 
 function countryMarkup(countries) {
+  refs.countriesList.innerHTML = '';
   if (countries.length >= 2 && countries.length <= 10) {
-    refs.countriesList.insertAdjacentHTML('beforeend', countryTmpl(countries));
+    refs.countriesList.insertAdjacentHTML('beforeend', countriesListTmpl(countries));
     return;
   }
   if (countries.length === 1) {
-    refs.countriesList.insertAdjacentHTML('beforeend', countriesListTmpl(countries));
+    refs.countriesList.insertAdjacentHTML('beforeend', countryTmpl(countries));
     return;
   }  
   if (countries.length >= 10) {
